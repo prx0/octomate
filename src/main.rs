96ae3145ce314854d44e39bf61f353fdca315e08
Maxmilien Di Dio <74690920+prx0@users.noreply.github.com>
2022-05-03 23:08:30 +0200

    Add more github features, add create team, gist and issue commands (#4)

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,16 @@ pub enum Error {
 
 async fn read_file(path: impl AsRef<Path>) -> Result<Vec<u8>, Error> {
     let bytes = fs::read(&path).await.context(IOSnafu {
-        path: String::from(path.as_ref().to_str().unwrap_or("")),
+        path: String::from(path.as_ref().to_str().unwrap_or_default()),
     })?;
     Ok(bytes)
 }
 
 #[derive(Debug)]
-struct Context<'a> {
-    batch: &'a Batch,
-    job: Option<&'a Job>,
-    step: Option<&'a Step>,
+pub struct Context<'a> {
+    pub batch: &'a Batch,
+    pub job: Option<&'a Job>,
+    pub step: Option<&'a Step>,
 }
 
 impl<'a> Context<'a> {
@@ -67,11 +67,11 @@ impl<'a> From<&Context<'a>> for Context<'a> {
     }
 }
 
-#[derive(Deserialize, Debug, Clone)]
-struct Batch {
-    version: String,
-    name: Option<String>,
-    jobs: Vec<Job>,
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct Batch {
+    pub version: String,
+    pub name: Option<String>,
+    pub jobs: Vec<Job>,
 }
 
 type BatchResult<Output, Err> = Vec<Vec<StepResult<Output, Err>>>;
@@ -100,12 +100,12 @@ impl TryFrom<&[u8]> for Batch {
     }
 }
 
-#[derive(Deserialize, Debug, Clone, Default)]
+#[derive(Deserialize, Debug, Clone, Default, PartialEq)]
 #[serde(rename_all = "kebab-case")]
-struct Job {
-    name: Option<String>,
-    on_repositories: Vec<Repository>,
-    steps: Vec<Step>,
+pub struct Job {
+    pub name: Option<String>,
+    pub on_repositories: Vec<Repository>,
+    pub steps: Vec<Step>,
 }
 
 impl Job {
@@ -119,26 +119,24 @@ impl Job {
             "job: {}",
             &self.name.clone().unwrap_or("UNAMED".to_string())
         );
-        let on_repositories = &self.on_repositories;
         let steps = &self.steps;
-        let steps_iter = steps.iter().map(|step| async move {
-            step.run(octocrab, on_repositories, &ctx.update_from_job(self))
-                .await
-        });
+        let steps_iter = steps
+            .iter()
+            .map(|step| async move { step.run(octocrab, &ctx.update_from_job(self)).await });
         futures::future::join_all(steps_iter).await
     }
 }
 
-#[derive(Deserialize, Debug, Clone)]
-struct Repository {
-    owner: String,
-    name: String,
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct Repository {
+    pub owner: String,
+    pub name: String,
 }
 
-#[derive(Deserialize, Debug, Clone, Default)]
-struct Step {
-    name: Option<String>,
-    runs: Vec<command::Command>,
+#[derive(Deserialize, Debug, Clone, Default, PartialEq)]
+pub struct Step {
+    pub name: Option<String>,
+    pub runs: Vec<command::Command>,
 }
 
 type StepResult<Output, Err> = Vec<Vec<Result<Output, Err>>>;
@@ -147,7 +145,6 @@ impl Step {
     pub async fn run(
         &self,
         octocrab: &Octocrab,
-        repositories: &[Repository],
         ctx: &Context<'_>,
     ) -> StepResult<command::Response, Error> {
         info!(
@@ -155,80 +152,213 @@ impl Step {
             &self.name.clone().unwrap_or("UNAMED".to_string())
         );
         let runs = &self.runs;
-        let runs_iter = runs.iter().map(|command| async move {
-            let on_repositories_iter = repositories.iter().map(|repository| async move {
-                command
-                    .run(
-                        octocrab,
-                        &repository.owner,
-                        &repository.name,
-                        &ctx.update_from_step(self),
-                    )
-                    .await
-            });
-            futures::future::join_all(on_repositories_iter).await
-        });
+        let runs_iter = runs
+            .iter()
+            .map(|command| async move { command.run(octocrab, &ctx.update_from_step(self)).await });
         futures::future::join_all(runs_iter).await
     }
 }
 
-mod command {
+pub mod command {
     use super::Context;
     use super::Error;
     use super::Octocrab;
     use crate::OctocrabSnafu;
-    use octocrab::models::Label;
+    use octocrab::models::{gists::Gist, issues::Issue, teams::Team, Label};
     use serde::Deserialize;
-    use snafu::{ResultExt, Snafu};
+    use snafu::ResultExt;
     use tracing::{debug, info};
 
-    #[derive(Deserialize, Debug, Clone)]
+    #[derive(Deserialize, Debug, Clone, PartialEq)]
     #[serde(rename_all = "kebab-case")]
     pub enum Command {
         CreateLabel(CreateLabelOptions),
+        CreateIssue(CreateIssueOptions),
+        CreateTeam(CreateTeamOptions),
+        CreateGist(CreateGistOptions),
     }
 
     impl Command {
         pub async fn run(
             &self,
             octocrab: &Octocrab,
-            owner: impl Into<String>,
-            repo: impl Into<String>,
             ctx: &Context<'_>,
-        ) -> Result<Response, Error> {
+        ) -> Vec<Result<Response, Error>> {
             match self {
-                Self::CreateLabel(options) => options.run(octocrab, owner, repo, ctx).await,
+                Self::CreateLabel(options) => options.run(octocrab, ctx).await,
+                Self::CreateIssue(options) => options.run(octocrab, ctx).await,
+                Self::CreateTeam(options) => options.run(octocrab, ctx).await,
+                Self::CreateGist(options) => options.run(octocrab, ctx).await,
             }
         }
     }
 
-    #[derive(Deserialize, Debug, Clone)]
+    #[derive(Deserialize, Debug, Clone, PartialEq)]
+    pub struct CreateGistOptions {
+        pub title: String,
+        pub content: String,
+        pub description: Option<String>,
+        pub public: Option<bool>,
+    }
+
+    impl CreateGistOptions {
+        pub async fn run(
+            &self,
+            octocrab: &Octocrab,
+            ctx: &Context<'_>,
+        ) -> Vec<Result<Response, Error>> {
+            debug!("{:?}", ctx);
+            let gist_res = octocrab
+                .gists()
+                .create()
+                .file(&self.title, &self.content)
+                .description(&self.description.clone().unwrap_or_default())
+                .public(self.public.unwrap_or(false))
+                .send()
+                .await
+                .context(OctocrabSnafu);
+
+            match gist_res {
+                Ok(gist) => vec![Ok(Response::CreateGist(gist))],
+                Err(err) => vec![Err(err)],
+            }
+        }
+    }
+
+    #[derive(Deserialize, Debug, Clone, PartialEq)]
+    pub struct CreateTeamOptions {
+        pub name: String,
+        pub description: Option<String>,
+        pub owner: String,
+        pub maintainers: Option<Vec<String>>,
+    }
+
+    impl CreateTeamOptions {
+        pub async fn run(
+            &self,
+            octocrab: &Octocrab,
+            ctx: &Context<'_>,
+        ) -> Vec<Result<Response, Error>> {
+            debug!("{:?}", ctx);
+            let team = match ctx.job {
+                None => Ok(Response::None),
+                Some(job) => {
+                    let on_repositories = &job.on_repositories;
+                    let repo_names: &Vec<String> = &on_repositories
+                        .iter()
+                        .map(|repository| repository.name.clone())
+                        .collect();
+
+                    let description = self.description.clone().unwrap_or_default();
+                    let maintainers = self.maintainers.clone().unwrap_or_default();
+
+                    let team_res = octocrab
+                        .teams(&self.owner)
+                        .create(&self.name)
+                        .description(&description)
+                        .maintainers(&maintainers)
+                        .repo_names(&repo_names)
+                        .send()
+                        .await
+                        .context(OctocrabSnafu);
+
+                    match team_res {
+                        Ok(team) => Ok(Response::CreateTeam(team)),
+                        Err(err) => Err(err),
+                    }
+                }
+            };
+            vec![team]
+        }
+    }
+
+    #[derive(Deserialize, Debug, Clone, PartialEq)]
+    pub struct CreateIssueOptions {
+        pub title: String,
+        pub body: String,
+        pub milestone: Option<u64>,
+        pub assignees: Option<Vec<String>>,
+        pub labels: Option<Vec<String>>,
+    }
+
+    impl CreateIssueOptions {
+        pub async fn run(
+            &self,
+            octocrab: &Octocrab,
+            ctx: &Context<'_>,
+        ) -> Vec<Result<Response, Error>> {
+            debug!("{:?}", ctx);
+            match ctx.job {
+                None => vec![Ok(Response::None)],
+                Some(job) => {
+                    let on_repositories = &job.on_repositories;
+                    let statements = on_repositories.iter().map(|repository| async move {
+                        let milestone = self.milestone.unwrap_or_default();
+                        let assignees = self.assignees.clone().unwrap_or_default();
+                        let labels = self.labels.clone().unwrap_or_default();
+
+                        let issue = octocrab
+                            .issues(&repository.owner, &repository.name)
+                            .create(&self.title)
+                            .body(&self.body)
+                            .milestone(milestone)
+                            .assignees(assignees)
+                            .labels(labels)
+                            .send()
+                            .await
+                            .context(OctocrabSnafu)?;
+
+                        Ok(Response::CreateIssue(issue))
+                    });
+
+                    let issues: Vec<Result<Response, Error>> =
+                        futures::future::join_all(statements).await;
+                    issues
+                }
+            }
+        }
+    }
+
+    #[derive(Deserialize, Debug, Clone, PartialEq)]
     pub struct CreateLabelOptions {
-        name: String,
-        color: String,
-        description: String,
+        pub name: String,
+        pub color: String,
+        pub description: String,
     }
 
     impl CreateLabelOptions {
         pub async fn run(
             &self,
             octocrab: &Octocrab,
-            owner: impl Into<String>,
-            repo: impl Into<String>,
             ctx: &Context<'_>,
-        ) -> Result<Response, Error> {
+        ) -> Vec<Result<Response, Error>> {
             debug!("{:?}", ctx);
-            let label = octocrab
-                .issues(owner, repo)
-                .create_label(&self.name, &self.color, &self.description)
-                .await
-                .context(OctocrabSnafu)?;
-            Ok(Response::CreateLabel(label))
+            match ctx.job {
+                None => vec![Ok(Response::None)],
+                Some(job) => {
+                    let on_repositories = &job.on_repositories;
+                    let statements = on_repositories.iter().map(|repository| async move {
+                        let label = octocrab
+                            .issues(&repository.owner, &repository.name)
+                            .create_label(&self.name, &self.color, &self.description)
+                            .await
+                            .context(OctocrabSnafu)?;
+                        Ok(Response::CreateLabel(label))
+                    });
+                    let labels: Vec<Result<Response, Error>> =
+                        futures::future::join_all(statements).await;
+                    labels
+                }
+            }
         }
     }
 
     pub enum Response {
         CreateLabel(Label),
+        CreateIssue(Issue),
+        CreateTeam(Team),
+        CreateGist(Gist),
+        None,
     }
 }
 
@@ -280,4 +410,82 @@ async fn main() {
         .run_batch_from_file("batch.yml")
         .await
         .expect("Unable to run batch from file");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use mockito::mock;
+    use octocrab::models::Label;
+
+    #[tokio::test]
+    async fn test_create_label() {
+        let url = &mockito::server_url();
+        let octocrab = octocrab::Octocrab::builder()
+            .personal_token("test".to_owned())
+            .base_url(url)
+            .unwrap()
+            .build()
+            .unwrap();
+
+        let _m = mock("POST", "/repos/owner1/repo1/labels")
+            .with_status(201)
+            .with_body(
+                r#"
+                {
+                    "id": 208045946,
+                    "node_id": "MDU6TGFiZWwyMDgwNDU5NDY=",
+                    "url": "https://api.github.com/repos/octocat/Hello-World/labels/bug",
+                    "name": "bug",
+                    "description": "Something isn't working",
+                    "color": "f29513",
+                    "default": true
+                }
+            "#,
+            )
+            .create();
+
+        let batch = r#"
+version: "1.0"
+name: Test
+jobs:
+  - name: "Perform some basics things for some repos"
+    on-repositories:
+      - owner: me
+        name: repo1
+    steps:
+      - name: Hello world!
+        runs:
+          - create-label:
+              name: "bug"
+              color: "f29513"
+              description: "Something isn't working"
+       "#
+        .as_bytes();
+
+        let batch = Batch::try_from(batch).unwrap();
+
+        assert_eq!(
+            batch,
+            Batch {
+                version: "1.0".to_owned(),
+                name: Some("Test".to_owned()),
+                jobs: vec![Job {
+                    name: Some("Perform some basics things for some repos".to_owned()),
+                    on_repositories: vec![Repository {
+                        owner: "me".to_owned(),
+                        name: "repo1".to_owned(),
+                    }],
+                    steps: vec![Step {
+                        name: Some("Hello world!".to_owned()),
+                        runs: vec![command::Command::CreateLabel(command::CreateLabelOptions {
+                            name: "bug".to_owned(),
+                            color: "f29513".to_owned(),
+                            description: "Something isn't working".to_owned(),
+                        })]
+                    }]
+                }]
+            }
+        );
+    }
 }
